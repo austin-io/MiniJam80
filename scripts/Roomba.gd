a1d291710d948252a1dee30874bb3ed9dc9c8690
austin <apoberezhnik23@gmail.com>
2021-05-15 15:11:02 -0700

    Roomba update

--- scripts/Roomba.gd
+++ scripts/Roomba.gd
@@ -2,6 +2,7 @@ extends KinematicBody
 
 var dir = Vector3();
 export (float) var SPEED = 2.0;
+onready var ray = $RayCast;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +11,8 @@ func _ready():
 	dir.x = randf();
 	dir.z = randf();
 	dir = dir.normalized();
+	
+	ray.cast_to = dir * SPEED;
 
 func _physics_process(delta):
 	move_and_slide(dir * SPEED, Vector3.UP);
