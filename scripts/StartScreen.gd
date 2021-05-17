d05d0adf7de6452cc9910849166ca0cff28c1a52
austin <apoberezhnik23@gmail.com>
2021-05-16 18:14:17 -0700

    Finished start menu

--- scripts/StartScreen.gd
+++ scripts/StartScreen.gd
@@ -2,3 +2,6 @@ extends Node2D
 
 func _on_TextureButton_pressed():
 	get_tree().change_scene("res://scenes/Level.tscn");
+
+func _on_IntroBtn_pressed():
+	get_tree().change_scene("res://scenes/IntroScene.tscn");
